64781ffb712d3aa753d2771a7e406bfe2bdd464b
vs <v.koltsov@gmail.com>
2014-10-10 15:03:51 +0400

    console fix

--- bin/console.php
+++ bin/console.php
@@ -1,18 +1,41 @@
 #!/usr/bin/env php
 <?php
+namespace Applejackyll;
+
 set_time_limit(0);
 date_default_timezone_set('Europe/Moscow');
 require_once __DIR__ . '/../vendor/autoload.php';
 
 define('DEFAULT_CONFIG_FILENAME', getcwd().DIRECTORY_SEPARATOR.'site.yaml');
 
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+class Console extends \Symfony\Component\Console\Application
+{
+    /**
+     * Gets the default input definition.
+     *
+     * @return InputDefinition An InputDefinition instance
+     */
+    protected function getDefaultInputDefinition()
+    {
+        return new \Symfony\Component\Console\Input\InputDefinition(array(
+            new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
+
+            new InputOption('--help',           '-h', InputOption::VALUE_NONE, 'Display this help message.'),
+            new InputOption('--quiet',          '-q', InputOption::VALUE_NONE, 'Do not output any message.'),
+            new InputOption('--config',         '-c', InputOption::VALUE_REQUIRED, 'Path to config file <site.yaml>', DEFAULT_CONFIG_FILENAME),
+            new InputOption('--version',        '-V', InputOption::VALUE_NONE, 'Display this application version.'),
+//            new InputOption('--ansi',           '',   InputOption::VALUE_NONE, 'Force ANSI output.'),
+            new InputOption('--no-ansi',        '',   InputOption::VALUE_NONE, 'Disable ANSI output.'),
+//            new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question.'),
+        ));
+    }
+}
 
 class StartCommand extends Command
 {
@@ -21,18 +44,10 @@ class StartCommand extends Command
         $this
             ->setName('start')
             ->setDescription('Start site generation')
-            ->addOption('config','c'
-                ,InputOption::VALUE_OPTIONAL
-                ,'Path to config file <site.yaml>'
-                ,DEFAULT_CONFIG_FILENAME
-            )
         ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        $config = $input->getArgument('config');
-//        $homedir = $input->getArgument('homedir');
-
         (new \Applejackyll\Applejackyll( $input->getOption('config') ))->parse();
     }
 }
@@ -64,12 +79,6 @@ class ServerCommand extends Command
                 ,'Specify document root <docroot> for built-in web server'
                 ,null
             )
-            ->addOption('config','c'
-                ,InputOption::VALUE_OPTIONAL
-                ,'Path to config file <site.yaml>'
-                ,DEFAULT_CONFIG_FILENAME
-            )
-
         ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -119,7 +128,7 @@ class ClearcacheCommand extends Command
 }
 
 
-$console = new Application('\\Applejackyll\\Console',\Applejackyll\Applejackyll::VERSION);
+$console = new Console('\\Applejackyll\\Console',\Applejackyll\Applejackyll::VERSION);
 $console->add(new StartCommand());
 $console->add(new ClearcacheCommand());
 $console->add(new ServerCommand());
